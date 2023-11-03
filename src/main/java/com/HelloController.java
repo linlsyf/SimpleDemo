e452ed4147992567f12fa0067f1f4e1c915beda3
linlsyf <linlsyf@gmail.com>
2023-11-03 17:35:41 +0800

    hello

---
+++ src/main/java/com/HelloController.java
@@ -0,0 +1,37 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+ public class HelloController {
+
+    @Autowired
+//    public DiscoveryClient client;
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
+    public String index() {
+//        java.util.List<ServiceInstance> instances = client.getInstances("hello-service");
+        return "jenkin test prohello";
+    }
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+//    public String index() {
+////        java.util.List<ServiceInstance> instances = client.getInstances("hello-service");
+//        return "Hello World eureka-product1";
+//    }
+
+
+
+//    @GetMapping("/hello")
+//        @HystrixCommand(fallbackMethod = "queryByIdFallback")
+//        public String hello() {
+//
+//            return "product-1";
+//        }
+
+
+
+    }
